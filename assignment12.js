ac833af2b8e3dbb79d723fc8d5d11a3766757231
pasinda <48270690+pasinda@users.noreply.github.com>
2019-05-10 16:17:19 -0700

    Add files via upload

---
+++ assignment12.js
@@ -0,0 +1,68 @@
+window.addEventListener("load",showTheTime,false);
+
+function showTheTime() {
+	var now = new Date();
+
+	document.getElementById("showTime").innerHTML = showTheHours(now.getHours()) + showZeroFilled(now.getMinutes()) + showZeroFilled(now.getSeconds()) + showAmPm();
+	setTimeout(showTheTime,1000);
+    
+    var sec = new Date.getSeconds
+    var elem = document.getElementById("myAnimation");   
+  var pos = 0;
+  var id = setInterval(frame, 9);
+  function frame() {
+    if (pos == 350) {
+      clearInterval(id);
+    } else {
+      pos=pos+(sec*3) 
+      elem.style.top = pos + 'px'; 
+      elem.style.left = pos + 'px'; 
+    }
+  }
+	
+	function showTheHours(theHour) {	
+		if (show24Hour() || (theHour > 0 && theHour < 13)) {
+			return theHour;
+		}
+		if (theHour == 0) {
+			return 12;
+		}
+		return theHour-12;
+	}
+	
+	function showZeroFilled(inValue) {
+		if (inValue > 9) {
+			return ":" + inValue;
+		}
+		return ":0" + inValue;
+	}
+
+	function show24Hour() {
+		return (document.getElementById("show24").checked);
+	}
+	
+	function showAmPm() {
+		if (show24Hour()) {
+			return "";
+		}
+		if (now.getHours() < 12) {
+			return " AM";
+		}
+		return " PM";
+	}
+
+function myMove() {
+  
+}
+
+
+
+
+
+
+
+
+}
+
+
+
