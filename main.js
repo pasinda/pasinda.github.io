ed2ed8c451c65fa85fd0b60226dc5e0be9a5dea3
pasinda <48270690+pasinda@users.noreply.github.com>
2019-05-12 02:47:52 -0700

    Update main.js

--- main.js
+++ main.js
@@ -15,15 +15,16 @@
                                        //Each card has value in this came since payer tries to get 21 as total to win
             for (var i = 0 ; i < values.length; i++)
             {
-                for(var x = 0; x < suits.length; x++)   /* Each card has havlues including kings, Queens , Jacks and Ace. This app have 
-                                                        given vlaues as 10 for kings, Queens , Jacks and 11 for each Ace. 
+                for(var x = 0; x < suits.length; x++)   /* Each card has vlues including kings, Queens , Jacks and Ace. This app have 
+                                                        given vlaues as 10 for kings, Queens , Jacks and 11 for each Ace. */
                 {
                     var weight = parseInt(values[i]);
                     if (values[i] == "J" || values[i] == "Q" || values[i] == "K")
                         weight = 10;
                     if (values[i] == "A")
                         weight = 11;
-                    var card = { Value: values[i], Suit: suits[x], Weight: weight };
+                    var card = { Value: values[i], Suit: suits[x], Weight: weight };/* Add each card details to end of the multivalued (multidimantional)
+                                                                                    array to use during the paly to calculate the winner  */
                     deck.push(card);
                 }
             }
@@ -73,14 +74,19 @@
                                                 */
         {
             // for 1000 turns
-            // switch the values of two random cards
+            // switch the values of two random cardsh
             for (var i = 0; i < 1000; i++)
             {
-                var location1 = Math.floor((Math.random() * deck.length));
-                var location2 = Math.floor((Math.random() * deck.length));
+                var location1 = Math.floor((Math.random() * deck.length)); //find the randon location (Random card)
+                var location2 = Math.floor((Math.random() * deck.length));// another random card from 52 cards
+                                                                          /* To be a perfect shuffle we have to remove the first locatin
+                                                                          (first selected card) from the deck and append updated the deck to 
+                                                                          for the next random selection and after the switching of data push the card
+                                                                          again to the deck. to limit the complexity of the program we arg going forward 
+                                                                          with this switching technique */      
                 var tmp = deck[location1];
 
-                deck[location1] = deck[location2];
+                deck[location1] = deck[location2]; // switch details of the cards  
                 deck[location2] = tmp;
             }
         }
@@ -89,43 +95,45 @@
         {
             document.getElementById('btnStart').value = 'Restart';
             document.getElementById("status").style.display="none";
-            // deal 2 cards to every player object
+                                                                        // deal 2 cards to every player object
             currentPlayer = 0;
-            createDeck();
-            shuffle();
-            createPlayers(2);
-            createPlayersUI();
-            dealHands();
+            createDeck();                                   //Calling for the array above with the card values and suit values push card details to array
+            shuffle();            // calling for shuffle cards 1000 times using random caed selection and detail switching
+            createPlayers(2);     // limit the payers to two and initialize the ecomponents
+            createPlayersUI();   //append HTML game page witch palyer and house details and points
+            dealHands();         // each calling of this function check the point status of the players and issue another 2 cards to the pasyer
             document.getElementById('player_' + currentPlayer).classList.add('active');
         }
 
         function dealHands()
         {
-            // alternate handing cards to each player
-            // 2 cards each
+                                                                                 // alternate handing cards to each player
+                                                                                 // 2 cards each
             for(var i = 0; i < 2; i++)
             {
-                for (var x = 0; x < players.length; x++)
-                {
+                for (var x = 0; x < players.length; x++)                /* limite the card to 2 for each player and 
+                                                                         total each payers point to check whether it reach the 21
+                                                                         and update the card deck with the issed cards to avoid the duplication */
+                {                                                      
                     var card = deck.pop();
                     players[x].Hand.push(card);
                     renderCard(card, x);
-                    updatePoints();
+                    updatePoints();                                                 
                 }
             }
 
-            updateDeck();
+            updateDeck();                        // to avoid the dulication of card issuance
         }
 
         function renderCard(card, player)
         {
-            var hand = document.getElementById('hand_' + player);
-            hand.appendChild(getCardUI(card));
+            var hand = document.getElementById('hand_' + player);   
+            hand.appendChild(getCardUI(card));                     // update isse card with details cand calling for card detail to display
         }
                                                                         
         function getCardUI(card)
-        {
-            var el = document.createElement('div');
+        {                                                       // assignning the card value with the card categoty image
+            var el = document.createElement('div');           
             var icon = '';
             if (card.Suit == 'Hearts')
             icon='&hearts;';
@@ -137,23 +145,23 @@
             icon = '&clubs;';
             
             el.className = 'card';
-            el.innerHTML = card.Value + '<br/>' + icon;
+            el.innerHTML = card.Value + '<br/>' + icon;  // ammend the game page (final project page)with card detail as well as the card sign 
             return el;
         }
 
-        // returns the number of points that a player has in hand
+                                                                     // returns the number of points that a player has in hand
         function getPoints(player)
         {
-            var points = 0;
-            for(var i = 0; i < players[player].Hand.length; i++)
+            var points = 0;                                      // initialize the point variable
+            for(var i = 0; i < players[player].Hand.length; i++) 
             {
-                points += players[player].Hand[i].Weight;
+                points += players[player].Hand[i].Weight;       //total points of the selected payer
             }
             players[player].Points = points;
             return points;
         }
 
-        function updatePoints()
+        function updatePoints()                                // update the payers points by ammend the game page by point
         {
             for (var i = 0 ; i < players.length; i++)
             {
@@ -164,8 +172,9 @@
 
         function hitMe()
         {
-            // pop a card from the deck to the current player
-            // check if current player new points are over 21
+                                                           /*  pop a card from the deck to the current player.
+                                                                and update the points cand card details assign to the current payer*/
+                                                            // check if current player new points are over 21
             var card = deck.pop();
             players[currentPlayer].Hand.push(card);
             renderCard(card, currentPlayer);
@@ -176,7 +185,7 @@
 
         function stay()
         {
-            // move on to next player, if any
+                                                                                        // move on to next player, if any
             if (currentPlayer != players.length-1) {
                 document.getElementById('player_' + currentPlayer).classList.remove('active');
                 currentPlayer += 1;
@@ -188,14 +197,14 @@
             }
         }
 
-        function end()
+        function end()                                                        
         {
-            var winner = -1;
+            var winner = -1;                                         // initialize the variable
             var score = 0;
 
             for(var i = 0; i < players.length; i++)
             {
-                if (players[i].Points > score && players[i].Points < 22)
+                if (players[i].Points > score && players[i].Points < 22)// calculate te payers points till it less than the 321
                 {
                     winner = i;
                 }
@@ -203,7 +212,7 @@
                 score = players[i].Points;
             }
 
-            document.getElementById('status').innerHTML = 'Winner: Player ' + players[winner].ID;
+            document.getElementById('status').innerHTML = 'Winner: Player ' + players[winner].ID; // show the status on game page
             document.getElementById("status").style.display = "inline-block";
         }
 
@@ -217,10 +226,10 @@
             }
         }
 
-        function updateDeck()
+        function updateDeck()                           // initially show the full size of the deck 52
         {
             document.getElementById('deckcount').innerHTML = deck.length;
-        }
+        }                                              //with ongoing game lengh will be less and less
 
         window.addEventListener('load', function(){
             createDeck();
