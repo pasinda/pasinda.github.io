813dd872dc102e4330f2f95f73e5b461698b014f
pasinda <48270690+pasinda@users.noreply.github.com>
2019-05-13 07:31:41 -0700

    Update ASSIGNMENT10.js

--- ASSIGNMENT10.js
+++ ASSIGNMENT10.js
@@ -7,6 +7,6 @@ function initImages() {
 }
 
 function newWindow() {
-	var imgName = "images/" + this.id + ".jpg"
+	var imgName = "images/" + this.id + ".JPG"
 	var imgWindow = window.open(imgName, "imgWin", "width=320,height=240,scrollbars=no")
 }
