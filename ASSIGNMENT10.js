5ebb66db613a2e5b0bed3566d8c6bc8bf88cea75
pasinda <48270690+pasinda@users.noreply.github.com>
2019-05-13 12:18:23 -0700

    Update ASSIGNMENT10.js

--- ASSIGNMENT10.js
+++ ASSIGNMENT10.js
@@ -1,12 +1,12 @@
 window.onload = initImages;
 
-function initImages() {
+function initImages() {                // makethe event by doudleclicking the thumbnail image
 	for (var i=0; i<document.images.length; i++) {
 		document.images[i].ondblclick = newWindow;
 	}
 }
 
-function newWindow() {
+function newWindow() {       // make the new window with the image and the dimentions
 	var imgName = "images/" + this.id + ".JPG"
 	var imgWindow = window.open(imgName, "imgWin", "width=320,height=240,scrollbars=no")
 }
