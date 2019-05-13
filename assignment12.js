1a0cb6e5b37c90627f8bcf615640fde799a77e3c
pasinda <48270690+pasinda@users.noreply.github.com>
2019-05-12 18:26:08 -0700

    Add files via upload

--- assignment12.js
+++ assignment12.js
@@ -1,23 +1,18 @@
 window.addEventListener("load",showTheTime,false);
+ 
+
+
+
 
 function showTheTime() {
 	var now = new Date();
-
+    myMove()
 	document.getElementById("showTime").innerHTML = showTheHours(now.getHours()) + showZeroFilled(now.getMinutes()) + showZeroFilled(now.getSeconds()) + showAmPm();
 	setTimeout(showTheTime,1000);
     
     var sec = new Date.getSeconds
     var elem = document.getElementById("myAnimation");   
-  var pos = 0;
-  var id = setInterval(frame, 9);
-  function frame() {
-    if (pos == 350) {
-      clearInterval(id);
-    } else {
-      pos=pos+(sec*3) 
-      elem.style.top = pos + 'px'; 
-      elem.style.left = pos + 'px'; 
-    }
+  
   }
 	
 	function showTheHours(theHour) {	
@@ -51,18 +46,22 @@ function showTheTime() {
 		return " PM";
 	}
 
+
 function myMove() {
-  
+  var elem1 = document.getElementById("animate");   
+  var pos = 0;
+  var id = setInterval(frame, 1);
+  function frame() {
+    if (pos == 350) {
+      clearInterval(id);
+      pos=0;
+      myMove();
+    } else {
+        
+      pos++; 
+     // elem.style.top = pos + "px"; 
+     elem1.style.left = pos + "px"; 
+    }
+  }
 }
-
-
-
-
-
-
-
-
-}
-
-
 
